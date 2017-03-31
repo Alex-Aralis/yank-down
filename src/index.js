2c4404985781a7c26b1cdcd62b69e45c51d53d96
Alex Aralis <alex.aralis@gmail.com>
2017-03-31 17:19:49 -0400

    have curryColletor collect all args from each call

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 export const curryCollector = (distance, acc = [], thunk = false) => {
   if (distance > 0)
     return curryCollector(distance - 1, acc,
-      arg => {
-        acc.push(arg);
+      (...args) => {
+        acc.push(args);
         return thunk || acc;
       }
     );
--- test/curryCollector.js
+++ test/curryCollector.js
@@ -4,10 +4,12 @@ import { curryCollector } from '../src';
 describe('curryCollector', function () {
   it('should collect numbers in order called', function () {
     const args = [1, 2, 3, 4];
+    const acc = args.map(num => [num]);
+
     let thunk = curryCollector(args.length);
 
     for (const num of args) thunk = thunk(num);
 
-    expect(thunk).to.deep.equal(args);
+    expect(thunk).to.deep.equal(acc);
   });
 });
