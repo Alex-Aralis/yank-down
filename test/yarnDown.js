6515fd7a9aa9dade74d3d86eea9281ccecec8f53
Alex Aralis <alex.aralis@gmail.com>
2017-03-31 20:30:21 -0400

    add more tests

--- test/yarnDown.js
+++ test/yarnDown.js
@@ -12,4 +12,46 @@ describe('yankDown', function () {
 
     expect(result).to.deep.equal([[2], [1], [3]]);
   });
+
+
+  it('should yank down last arg', function () {
+    const args = [1, 2, 3].map(entry => [entry]);
+    let thunk = collect(3);
+
+    thunk = yankDown(thunk)(2);
+
+    const result = apply(thunk)(args);
+
+    expect(result).to.deep.equal([[2], [3], [1]]);
+  });
+
+  it('should return the same function when yanking 0', function () {
+    const args = [1, 2, 3].map(entry => [entry]);
+    let thunk = collect(3);
+
+    thunk = yankDown(thunk)(0);
+
+    const result = apply(thunk)(args);
+
+    expect(result).to.deep.equal([[1], [2], [3]]);
+  });
+
+  it('should work with multiple args', function () {
+    const args = [[1], [2, 3], [4, 5]];
+    let thunk = collect(3);
+
+    thunk = yankDown(thunk)(1);
+
+    const result = apply(thunk)(args);
+
+    expect(result).to.deep.equal([[2, 3], [1], [4, 5]]);
+  });
+
+  it('should work with depth 1 thunks', function () {
+    const thunk = arg => arg;
+
+    yankDown(thunk)(0);
+
+    expect(thunk('foo')).to.equal('foo');
+  });
 });
